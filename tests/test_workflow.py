610cfc355338668a682c4cf2ad2af5e9f554ff23
FewerElk <120582916+FewerElk@users.noreply.github.com>
2024-08-10 11:45:28 +0200

    Update test_workflow.py

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -1,7 +1,9 @@
-# agree EULA (neutralized to prevent fail)
-#with open("eula.txt", "w") as ef:
-#  ef.write("eula=true")
 
-#start the server
-import os
-os.system("python main.py")
+def test_app():
+    # agree EULA (neutralized to prevent fail)
+    #with open("eula.txt", "w") as ef:
+    #  ef.write("eula=true")
+
+    #start the server
+    import os
+    os.system("python main.py")
