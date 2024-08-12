b869cd4c17d220d06e2579d9acb760ace585e237
FewerElk <120582916+FewerElk@users.noreply.github.com>
2024-08-12 13:18:51 +0200

    Trying to remove some warning lines in lint check (6)

--- libs/mojangapi.py
+++ libs/mojangapi.py
@@ -1,13 +1,15 @@
-#=========================
+# =========================
 # BEACONMC 1.19.4
-#=========================
+# =========================
 # Mojang API
 
 import requests
 import json
 
+
 class MinecraftAccountVerificationError(Exception):
     pass
+
 
 class Accounts(object):
     def exists(self, username):
@@ -24,35 +26,36 @@ class Accounts(object):
         '''Check authenticity of an account'''
         try:
             with open("libs/requests/authcheck.json", "r") as f:
-                import json
                 data = json.loads(f.read())
 
             response = requests.post(url="https://authserver.mojang.com/authenticate", json=data)
             response.raise_for_status()  # Raise an exception for bad responses (4xx or 5xx status codes)
-            print(json_data = response.json())
-            
+            print(json_data=response.json())
+
             return True
         except Exception as e:
             print(e)
             return False
-            
+
     def authenticate(self, username, uuid):
         try:
             response = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{username}")
-        
+            
             if response.status_code == 200:
                 profile = response.json()
-            
+
                 if profile['id'] == uuid.replace('-', ''):
                     return (True, "")
                 else:
                     return (False, f"UUID doesn't match. Receivt: {uuid}, expected: {profile['id']}.")
             else:
                 return (False, f"This username doesn't exist ({response.status_code}).")
-    
+
         except Exception as e:
             raise MinecraftAccountVerificationError from e
-        
+
+
 if __name__ == "__main__":
     acc = Accounts()
     acc.check("EletrixTime")
+    
