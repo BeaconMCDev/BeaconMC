b9c4d07cc0ba6286f7ef54ba1966d14e2ee26ffb
FewerElk <120582916+FewerElk@users.noreply.github.com>
2024-08-12 18:32:24 +0200

    Same

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -1,15 +1,14 @@
-
 def test_app():
     # check if eula is not accepted in the build, to prevent starting the server without accepting it.
     with open("eula.txt", "r") as eula_file:
-        eula=eula_file.read().split()
+        eula = eula_file.read().split()
         if "eula=true" in eula:
             raise RuntimeError("Eula is accepted by default, change eula to false in eula.txt to continue.")
-    
+
     # agree EULA (neutralized to prevent fail)
-    #with open("eula.txt", "w") as ef:
+    # with open("eula.txt", "w") as ef:
     #  ef.write("eula=true")
 
-    #start the server
+    # start the server
     import os
     os.system("python main.py")
