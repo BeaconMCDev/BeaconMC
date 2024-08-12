7adb24288f0921bb53f23e188113496ca6645f39
FewerElk <120582916+FewerElk@users.noreply.github.com>
2024-08-12 18:42:59 +0200

    Always the same thing

--- plugins/example/pluginsystem.py
+++ plugins/example/pluginsystem.py
@@ -1,20 +1,20 @@
 """The plugin api that allow plugin creation"""
 
-#class
+# class
 class BeaconMCPlugin(object):
-    def __init__(self, name:str, author:str, version:str, srv_version:str, server:object=None):
+    def __init__(self, name: str, author: str, version: str, srv_version: str, server: object=None):
         self.name = name
         self.author = author
         self.server = server
         self.version = version
         self.srv_version = srv_version
 
-        #NOTE ABOUT THE STATE ATTRIBUTE : 0 is not loaded, 1 is loaded, -1 is disabled (suite of an error or a server stop)
+        # NOTE ABOUT THE STATE ATTRIBUTE : 0 is not loaded, 1 is loaded, -1 is disabled (suite of an error or a server stop)
         self.state = 0
 
     # Plugin.on_load() and Plugin.on_unload() are not here, and have to be included in the plugin created by someone
 
-    def set_server(self, srv:object):
+    def set_server(self, srv: object):
         self.server = srv
 
     def _on_load(self):
@@ -30,22 +30,22 @@ class BeaconMCPlugin(object):
 
     def disable(self):
         self.state = -1
-        self.LOCK = True   #used to block the bypasses
+        self.LOCK = True   # used to block the bypasses
 
-    def on_player_join(self, player:str):
+    def on_player_join(self, player: str):
         pass
 
-    def on_player_leave(self, player:str):
+    def on_player_leave(self, player: str):
         pass
 
-    def on_player_death(self, player:str, message:str):
+    def on_player_death(self, player: str, message: str):
         pass
 
-    def on_player_reaspawn(self, player:str):
+    def on_player_reaspawn(self, player: str):
         pass
 
-    def on_message(self, messag:str, sourcev):
+    def on_message(self, messag: str, sourcev):
         pass
 
-    def on_mp(self, message:str, source:str, addressee:str):
+    def on_mp(self, message: str, source: str, addressee: str):
         pass
