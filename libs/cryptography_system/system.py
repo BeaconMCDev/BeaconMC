183e32e4ab0d7013c21ba029f5ca2bed20ac92be
FewerElk <120582916+FewerElk@users.noreply.github.com>
2024-08-15 12:19:02 +0200

    Update system.py

--- libs/cryptography_system/system.py
+++ libs/cryptography_system/system.py
@@ -8,16 +8,34 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
 
 class CryptoSystem(object):
+    KEY_HIDDEN_MESSAGE = "403\nKEY HIDDEN FOR SECURITY REASONS. IT WILL BE WRITTEN HERE ON SERVER STOP (final action to prevent plugins access it)."
+    
     def __init__(self, server):
         """Load public and private keys. Hide private key for security reason."""
         self.server = server
         try:
             with open(".private_key.pem", "rb") as skf:
-                self._PRIVATE_KEY = skf.read()
+                self._private_key = skf.read()
             with open(".private_key.pem", "wb") as skf:
-                skf.write("403\nKEY HIDDEN FOR SECURITY REASONS. IT WILL BE WRITTEN HERE ON SERVER STOP (final action to prevent plugins access it).")
+                skf.write(self.KEY_HIDDEN_MESSAGE)
             with open("public_key.pem", "rb") as pkf:
-                self.PUBLIC_KEY = pkf.read()
+                self.public_key = pkf.read()
+            if self.null_key():
+                self.generate_keys()
         except FileNotFoundError:
-            ...
-            # TODO: generate keys
+            self.generate_keys()
+
+    def null_keys(self) -> bool:
+        """Check if keys exists or not (in project variables)."""
+        null = ("", " ", "None", "none", "null", "Null")
+        if self._private_key == self.KEY_HIDDEN_MESSAGE:
+            # The initial key was not restaured.
+            return True
+        if self._private_key in null:
+            return True
+        if self.public_key in null:
+            return True
+        return False
+
+    def generate_keys(self):
+        ...
