dbd1e52624dcfda20b519465d406996d8643e4f3
FewerElk <120582916+FewerElk@users.noreply.github.com>
2024-08-19 09:27:47 +0200

    Create BeaconMCPlugin.py

---
+++ utils/plugins/BeaconMCPlugin.py
@@ -0,0 +1,12 @@
+class BeaconMCPlugin(object):
+    def __init__(self, server):
+        self.server = server
+
+    def onEnable(self):
+      pass
+
+    def onDisable(self):
+        pass
+
+    def onPlayerJoinEvent(self, player):
+        pass
