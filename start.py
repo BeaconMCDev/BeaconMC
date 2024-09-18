742c40cf2202d599a56b59cd6bf690f4a7997e5b
FewerElk <120582916+FewerElk@users.noreply.github.com>
2024-09-18 11:10:51 +0200

    Update start.py

--- start.py
+++ start.py
@@ -1,1 +1,20 @@
+# BeaconMC installation and boot file
 
+# Import
+import os
+
+# Check structure
+if os.path.exists("config.json"):
+    ...
+
+# Check requirements
+...
+
+# install server if required
+...
+
+# start
+with open ("main.py", "r") as f:
+    code = f.read()
+
+exec(code, __name__="__main__")
