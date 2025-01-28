70dc6744c9d23522df9df2e49b45a9df95c3267a
LiveInGround <129398571+LiveInGround@users.noreply.github.com>
2025-01-28 19:56:18 +0100

    Update crash_gen.py

--- libs/crash_gen.py
+++ libs/crash_gen.py
@@ -14,7 +14,7 @@ import json
 import platform
 
 TOTAL_PLUGIN = 0
-def gen_crash_report(SERVER_VERSION, BMC_VERSION):
+def gen_crash_report(SERVER_VERSION, BMC_VERSION, e):
     global TOTAL_PLUGIN
     with open("config.json", "r") as f:
         config = json.loads(f.read())
@@ -33,7 +33,7 @@ def gen_crash_report(SERVER_VERSION, BMC_VERSION):
         for p in os.listdir("plugins"):
             plugin_list += f"- {p}\n"
             TOTAL_PLUGIN += 1
-        json_info = json.dumps({"mc_version": SERVER_VERSION,"bmc_version": BMC_VERSION, "os_name": os.name,"date": datetime.now().isoformat(),"python_version": sys.version,"total_plugin": TOTAL_PLUGIN, "online_mode": online_mode,"traceback_error": traceback.format_exc()})
+        json_info = json.dumps({"mc_version": SERVER_VERSION,"bmc_version": BMC_VERSION, "os_name": os.name,"date": datetime.now().isoformat(),"python_version": sys.version,"total_plugin": TOTAL_PLUGIN, "online_mode": online_mode,"traceback_error": traceback.format_exc(e)})
         f.write(f"""
 =========================================
         BEACON-MC CRASH REPORT
@@ -41,7 +41,7 @@ def gen_crash_report(SERVER_VERSION, BMC_VERSION):
 Something went wrong, please submit the report on the issue tracker.
 https://github.com/BeaconMCDev/BeaconMC/issues/new?assignees=&labels=bug&projects=&template=bug_report.md&title=
 Traceback :
-{traceback.format_exc()}
+{traceback.format_exc(e)}
 =========================================
 OS : {platform.system()}
 Python Version : {sys.version}
