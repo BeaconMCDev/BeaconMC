448662418b79d32b92b0424c2b2b1d1fa8d1fc79
LiveInGround <129398571+LiveInGround@users.noreply.github.com>
2025-08-10 10:09:59 +0200

    Update registry.py

--- src/registry/registry.py
+++ src/registry/registry.py
@@ -1,6 +1,11 @@
 class Registry(object):
-    def __init__(self, namespace:str, name:str):
+    def __init__(self, namespace:str, name:str, base_ptn:dict):
         self.namespace = namespace
         self.name = name
         self.displayName = f"{namespace}:{name}"
+        self._BASE_PTN = base_ptn
+        ...
+        
+    def get_entity_name(id:int):
+        return self._BASE_PTN[id]
         
