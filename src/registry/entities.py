e1bd0b34eca611e019ec37ee2d6e023b7ba34d24
LiveInGround <129398571+LiveInGround@users.noreply.github.com>
2025-08-13 16:52:31 +0200

    Update entities.py

--- src/registry/entities.py
+++ src/registry/entities.py
@@ -15,7 +15,30 @@ class EntitiesRegistry(regist.Registry):
         8: "bamboo_chest_raft",
         9: "bamboo_raft",
         10: "bat",
-        11: "bee"
+        11: "bee",
+        12: "birch_boat",
+        13: "birch_chest_boat",
+        14: "blaze",
+        15: "block_display",
+        16: "bogged",
+        17: "breeze",
+        18: "breeze_wind_charge",
+        19: "camel",
+        20: "cat",
+        21: "cave_spider",
+        22: "cherry_boat",
+        23: "cherry_chest_boat",
+        24: "chest_minecart",
+        25: "chicken",
+        26: "cod"
+        27: "command_block_minecart",
+        28: "cow",
+        29: "creaking",
+        30: "creeper",
+        31: "dark_oak_boat",
+        32: "dark_oak_chest_boat",
+        33: "dolphin",
+        34: "donkey"
     }
     ...
     
