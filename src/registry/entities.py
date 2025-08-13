9be091b5ea1edec2da2433a3930f49b08781379b
LiveInGround <129398571+LiveInGround@users.noreply.github.com>
2025-08-13 18:35:20 +0200

    Update entities.py

--- src/registry/entities.py
+++ src/registry/entities.py
@@ -38,7 +38,33 @@ class EntitiesRegistry(regist.Registry):
         31: "dark_oak_boat",
         32: "dark_oak_chest_boat",
         33: "dolphin",
-        34: "donkey"
+        34: "donkey",
+        35: "dragon_fireball",
+        36: "drowned",
+        37: "egg",
+        38: "elder_guardian",
+        39: "enderman",
+        40: "endermite",
+        41: "ender_dragon",
+        42: "ender_pearl",
+        43: "end_crystal",
+        44: "evoker",
+        45: "evoker_fangs",
+        46: "experience_bottle",
+        47: "experience_orb", # DO NOT SPAWN WITH ˋSPAWN ENTITY` PACKET
+        48: "eye_of_ender",
+        49: "falling_block",
+        50: "fireball",
+        51: "firework_rocket",
+        52: "fox",
+        53: "frog",
+        54: "furnace_minecart",
+        55: "ghast",
+        56: "happy_ghast",
+        57: "giant",
+        58: "glow_item_frame",
+        59: "glow_squid",
+        60: "goat"
     }
     ...
     
