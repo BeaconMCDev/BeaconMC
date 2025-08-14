013f8af6c61890e6265f014ede11f48c8da8a113
LiveInGround <129398571+LiveInGround@users.noreply.github.com>
2025-08-14 14:39:14 +0200

    Update BeaconMCPlugin.py

--- utils/plugins/BeaconMCPlugin.py
+++ utils/plugins/BeaconMCPlugin.py
@@ -1,6 +1,11 @@
 class BeaconMCPlugin(object):
     def __init__(self, server):
         self.server = server
+        for name in dir(self):
+            method = getattr(self, name)
+            if hasattr(method, "_mc_event"):
+                # register
+                # self.server.register(method._mc_event, method)
 
     def onEnable(self):
       pass
