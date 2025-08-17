3871a26ad1a0cdbff01614bcc4834c4ab3645b80
LiveInGround <129398571+LiveInGround@users.noreply.github.com>
2025-08-17 20:44:56 +0200

    Update blocks.py

--- src/registry/blocks.py
+++ src/registry/blocks.py
@@ -1,10 +1,8 @@
-import registry
-
-class BlocksRegistry(registry.Registry):
+class BlocksRegistry():
     _BASE_PTN = {
-        0: "air"
-        ... # i need to find the correct source
+        
     }
     
-    def __init__(self):
-        super().__init__("minecraft", "blocks", self._BASE_PTN)
+    name = "blocks"
+    namespace = "minecraft"
+    displayName = f"{namespace}:{name}"
