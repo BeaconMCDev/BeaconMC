6782906d638f588418fcc84fbd3254e49cf3b8e6
LiveInGround <129398571+LiveInGround@users.noreply.github.com>
2025-08-19 09:48:22 +0200

    Add files via upload
    Adding in dev tools the block class generator to generate block classes

---
+++ src/.ignore_dev/block_class_generator.py
@@ -0,0 +1,28 @@
+import json
+import pprint
+
+with open("blocks.json", "r") as f:
+    data = json.load(f)
+
+for name, value in data.items():
+    namespace, n = name.split(":")
+    definition = value["definition"]
+    defstr = ""
+    for k, v in definition.items():
+        defstr += f"{k} = {json.dumps(v)}"
+        defstr += "\n    "
+
+    classdata = f"""from block import Block
+from ..location import Location
+class {''.join(word.capitalize() for word in n.split('_'))}(Block):
+    
+    {defstr}
+    PROPERTIES = {repr(value["properties"])}
+    def __init__(self, location:Location, properties={{}}):
+        super().__init__("{namespace}", "{n}", location, properties)"""
+
+    with open(f"{n}.py", "w") as f:
+        f.write(classdata)
+
+    break
+print("Opt effectuée")
